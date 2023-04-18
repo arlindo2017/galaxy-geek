dd3eaeefd9a9c767245f6bee77a520facc02fda3
Arlindo Pinto <perensen@gmail.com>
2023-04-18 18:32:23 -0400

    tweaked posts layout

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -35,7 +35,7 @@ router.get("/", async (req, res) => {
         ],
       ],
       order: [["view_count", "DESC"]],
-      limit: 6,
+      // limit: 6,
     });
 
     const userCountData = await User.count();
